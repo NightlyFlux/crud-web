f6a6913a275e670937e0d0110f3e498d459e03e1
NightlyFlux <nightlyflux@gmail.com>
2020-12-22 22:18:56 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,213 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import http from './api'
+import 'antd/dist/antd.css'
+import { Button, Form, Input, Layout, Modal, Radio } from 'antd'
+import { TeamOutlined, UserAddOutlined } from '@ant-design/icons'
+import { Content, Footer, Header } from 'antd/lib/layout/layout'
+import Title from 'antd/lib/typography/Title'
+import EmployeeTable from './components/EmployeeTable'
 
-function App() {
+const App = () => {
+  const [form] = Form.useForm()
+  const [employees, setEmployees] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [currentId, setCurrentId] = useState(null)
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const addEmployee = ({ name, email, address, phone, role_id }) => {
+    setIsLoading(true)
+    http
+      .post('/employees', {
+        name,
+        email,
+        address,
+        phone,
+        role_id,
+      })
+      .then(() => {
+        loadEmployees()
+        closeModal()
+        setIsLoading(false)
+      })
+  }
+
+  const loadEmployees = () => {
+    setIsLoading(true)
+    http.get('/employees').then(({ data }) => {
+      setEmployees(data)
+      setIsLoading(false)
+    })
+  }
+
+  const updateEmployee = ({ name, email, address, phone, role_id }) => {
+    setIsLoading(true)
+    http
+      .patch(`/employees/${currentId}`, {
+        name,
+        email,
+        address,
+        phone,
+        role_id,
+      })
+      .then(({ status }) => {
+        debugger
+        if (status === 200) {
+          loadEmployees()
+          closeModal()
+          setIsLoading(false)
+        }
+      })
+      .catch(() => {
+        console.log('ERROR')
+      })
+  }
+
+  const deleteEmployee = (id) => {
+    setIsLoading(true)
+    http.delete(`/employees/${id}`).then(() => {
+      const updatedEmployees = employees.filter(
+        (employee) => employee.id !== id
+      )
+      setEmployees(updatedEmployees)
+      setIsLoading(false)
+    })
+  }
+
+  useEffect(() => {
+    loadEmployees()
+  }, [setEmployees])
+
+  useEffect(() => {
+    if (currentId !== null) {
+      form.setFieldsValue(
+        employees.find((employee) => employee.id === currentId)
+      )
+      showModal()
+    }
+  }, [currentId, employees, form])
+
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const closeModal = () => {
+    setIsModalVisible(false)
+    form.resetFields()
+    setCurrentId(null)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Layout>
+        <Header align='center' style={{ paddingTop: '0.5rem' }}>
+          <Title style={{ color: 'white' }}>
+            <TeamOutlined /> Employees CRUD
+          </Title>
+        </Header>
+
+        <Content style={{ padding: '0 1rem' }}>
+          <Button
+            type='primary'
+            icon={<UserAddOutlined />}
+            style={{ margin: '1rem 0' }}
+            onClick={showModal}>
+            Add Employee
+          </Button>
+          <EmployeeTable
+            employees={employees}
+            loading={isLoading}
+            showModal={showModal}
+            setCurrentId={setCurrentId}
+            deleteEmployee={deleteEmployee}
+          />
+        </Content>
+
+        <Footer align='center'>
+          Made by
+          <Button
+            type='link'
+            href='https://nightlyflux.tk'
+            target='_blank'
+            size='small'>
+            Salvador Gómez
+          </Button>
+        </Footer>
+      </Layout>
+      <Modal
+        title={`${currentId ? 'Edit' : 'Add'} Employee`}
+        visible={isModalVisible}
+        okText={currentId ? 'Save changes' : 'Add'}
+        onOk={() => {
+          form
+            .validateFields()
+            .then((values) => {
+              currentId ? updateEmployee(values) : addEmployee(values)
+            })
+            .catch((info) => {
+              console.log('Validation Failed:', info)
+            })
+        }}
+        onCancel={closeModal}
+        centered>
+        <Form
+          form={form}
+          layout='vertical'
+          name='EmployeeForm'
+          initialValues={{ role_id: 1 }}>
+          <Form.Item
+            name='name'
+            label='Name'
+            rules={[
+              {
+                required: true,
+                message: 'Please input your name!',
+              },
+            ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name='email'
+            label='Email'
+            rules={[
+              {
+                required: true,
+                message: 'Please input your email!',
+              },
+            ]}>
+            <Input type='email' />
+          </Form.Item>
+          <Form.Item
+            name='address'
+            label='Address'
+            rules={[
+              {
+                required: true,
+                message: 'Please input your address!',
+              },
+            ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name='phone'
+            label='Phone number'
+            rules={[
+              {
+                required: true,
+                message: 'Please input your phone number!',
+              },
+            ]}>
+            <Input type='tel' />
+          </Form.Item>
+          <Form.Item name='role_id' label='Role' style={{ marginBottom: 0 }}>
+            <Radio.Group>
+              <Radio value={1}>Admin</Radio>
+              <Radio value={2}>User</Radio>
+            </Radio.Group>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/EmployeeTable.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import { Button, Popconfirm, Table } from 'antd'
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons'
+
+const EmployeeTable = ({
+  employees,
+  loading,
+  showModal,
+  setCurrentId,
+  deleteEmployee,
+}) => {
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Phone number',
+      dataIndex: 'phone',
+      key: 'phone',
+    },
+    {
+      title: 'Role',
+      dataIndex: 'role',
+      key: 'role',
+    },
+    {
+      align: 'center',
+      title: 'Edit',
+      key: 'edit',
+      render: (record) =>
+        employees.length > 0 ? (
+          <Button
+            type='primary'
+            shape='round'
+            icon={<EditOutlined />}
+            onClick={() => {
+              setCurrentId(record.id)
+            }}
+          />
+        ) : null,
+    },
+    {
+      align: 'center',
+      title: 'Delete',
+      key: 'delete',
+      render: (record) =>
+        employees.length > 0 ? (
+          <Popconfirm
+            title='Are you sure?'
+            onConfirm={() => deleteEmployee(record.id)}>
+            <Button
+              type='dashed'
+              danger
+              shape='round'
+              icon={<DeleteOutlined />}
+            />
+          </Popconfirm>
+        ) : null,
+    },
+  ]
+  return (
+    <Table
+      dataSource={employees}
+      columns={columns}
+      loading={loading}
+      pagination={false}
+      rowKey='id'
+    />
+  )
+}
+
+export default EmployeeTable
